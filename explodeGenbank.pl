2ec73d4313147956da88ab893c3058f7cc8c1bc6
Michael.Imelfort <mike@mikeimelfort.com>
2012-06-20 13:59:56 +1000

    first commit

---
+++ explodeGenbank.pl
@@ -0,0 +1,353 @@
+#!/usr/bin/env perl
+###############################################################################
+#
+#    explodeGenbank.pl
+#    
+#    Separate a multiple genbank file into separate records
+#
+#    Copyright (C) Michael Imelfort
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+#pragmas
+use strict;
+use warnings;
+
+#core Perl modules
+use Getopt::Long;
+use Carp;
+use File::Path qw(make_path);
+
+#CPAN modules
+
+#locally-written modules
+
+BEGIN {
+    select(STDERR);
+    $| = 1;
+    select(STDOUT);
+    $| = 1;
+}
+
+# edit here to log all external commands 
+my $global_log_commands = 0;
+
+# ext command failure levels
+use constant {
+    IGNORE_FAILURE => 0,
+    WARN_ON_FAILURE => 1,
+    DIE_ON_FAILURE => 2
+};
+
+# get input params and print copyright
+printAtStart();
+my $global_options = checkParams();
+
+######################################################################
+# CODE HERE
+######################################################################
+my $gb_fh = openRead($global_options->{'genbank'});
+my $in_gb = 0;
+my $gb_file_open = 0;
+my @gb_buffer = ();
+my $gbr_fh;
+my $ex_dir = $global_options->{'genbank'}."_exploded"; 
+make_path($ex_dir);
+while(<$gb_fh>)
+{
+    if($_ =~ /^LOCUS/)
+    {
+        # new GB record
+        $in_gb = 1; 
+        # no file opened yet, push onto the buffer
+        push @gb_buffer, $_;
+    }
+    elsif($_ =~ /^\/\//)
+    {
+        # end of GB record
+        if(1 == $gb_file_open)
+        {
+            print $gbr_fh $_;
+            close $gbr_fh;
+            $gb_file_open = 0;
+            $in_gb = 0;
+        }
+    }
+    elsif($_ =~ /^VERSION/)
+    {
+        # We now have enough information to open the file
+        my $line = $_;
+        chomp $line;
+        $line =~ s/VERSION[ ]*//;
+        $line =~ s/ .*//;
+        $line .= ".gb";
+        $gbr_fh = openWrite($ex_dir."/".$line);
+        $gb_file_open = 1;
+        foreach my $b_line (@gb_buffer)
+        {
+            print $gbr_fh $b_line;
+        }
+        print $gbr_fh $_;
+        $#gb_buffer = -1;
+    }    
+    elsif(1 == $gb_file_open)
+    {
+        print $gbr_fh $_;
+    }
+    elsif(1 == $in_gb)
+    {
+        # no file opened yet, push onto the buffer
+        push @gb_buffer, $_;
+    }
+    
+}
+close $gb_fh;
+######################################################################
+# CUSTOM SUBS
+######################################################################
+
+######################################################################
+# TEMPLATE SUBS
+
+######################################################################
+# PARAMETERS
+
+sub checkParams {
+    #-----
+    # Do any and all options checking here...
+    #
+    my @standard_options = ( "help|h+", "genbank|g:s");
+    my %options;
+
+    # Add any other command line options, and the code to handle them
+    # 
+    GetOptions( \%options, @standard_options );
+
+    # if no arguments supplied print the usage and exit
+    #
+    exec("pod2usage $0") if (0 == (keys (%options) ));
+
+    # If the -help option is set, print the usage and exit
+    #
+    exec("pod2usage $0") if $options{'help'};
+
+    # Compulsory items
+    #if(!exists $options{''} ) { printParamError (""); }
+
+    return \%options;
+}
+
+sub printParamError
+{
+    #-----
+    # What to do if there's something wrong with a parameter
+    #  
+    my ($error) = @_;  
+    print "**ERROR: $0 : $error\n"; exec("pod2usage $0");
+}
+
+sub overrideDefault
+{
+    #-----
+    # Set and override default values for parameters
+    #
+    my ($default_value, $option_name) = @_;
+    if(exists $global_options->{$option_name}) 
+    {
+        return $global_options->{$option_name};
+    }
+    return $default_value;
+}
+
+######################################################################
+# FILE IO
+
+sub openWrite
+{
+    #-----
+    # Open a file for writing
+    #
+    my ($fn) = @_;
+    open my $fh, ">", $fn or croak "**ERROR: could not open file: $fn for writing $!\n";
+    return $fh;
+}
+
+sub openRead
+{   
+    #-----
+    # Open a file for reading
+    #
+    my ($fn) = @_;
+    open my $fh, "<", $fn or croak "**ERROR: could not open file: $fn for reading $!\n";
+    return $fh;
+}
+
+######################################################################
+# EXTERNAL COMMANDS
+#
+# checkAndRunCommand("ls", {
+#                          -a => ""
+#                          }, 
+#                          WARN_ON_FAILURE);
+
+sub checkFileExists {
+    #-----
+    # Does a file exists?
+    #
+    my ($file) = @_;
+    unless(-e $file) {
+        croak "**ERROR: $0 : Cannot find:\n$file\n";
+    }
+}
+
+sub logExternalCommand
+{
+    #-----
+    # Log a command line command to the command line!
+    #
+    if(1 == $global_log_commands) {
+        print $_[0], "\n";
+    }
+}
+
+sub isCommandInPath
+{
+    #-----
+    # Is this command in the path?
+    #
+    my ($cmd, $failure_type) = @_;
+    if (system("which $cmd |> /dev/null")) {
+        handleCommandFailure($cmd, $failure_type);
+    }
+}
+
+sub runExternalCommand
+{
+    #-----
+    # Run a command line command on the command line!
+    #
+    my ($cmd) = @_;
+    logExternalCommand($cmd);
+    system($cmd);
+}
+
+sub checkAndRunCommand
+{
+    #-----
+    # Run external commands more sanelier
+    #
+    my ($cmd, $params, $failure_type) = @_;
+    
+    isCommandInPath($cmd, $failure_type);
+    
+    # join the parameters to the command
+    my $param_str = join " ", map {formatParams($_)} @{$params};
+    
+    my $cmd_str = $cmd . " " . $param_str;
+    
+    logExternalCommand($cmd_str);
+
+    # make sure that all went well
+    if (system($cmd_str)) {
+         handleCommandFailure($cmd_str, $failure_type)
+    }
+}
+
+sub formatParams {
+
+    #---------
+    # Handles and formats the different ways of passing parameters to 
+    # checkAndRunCommand
+    #
+    my $ref = shift;
+    
+    if (ref($ref) eq "ARRAY") {
+        return join(" ", @{$ref});
+    } elsif (ref($ref) eq "HASH") {
+        return join(" ", map { $_ . " " . $ref->{$_}} keys %{$ref});
+    }
+    croak 'The elements of the $params argument in checkAndRunCommand can ' .
+        'only contain references to arrays or hashes\n';
+}
+
+
+sub handleCommandFailure {
+    #-----
+    # What to do when all goes bad!
+    #
+    my ($cmd, $failure_type) = @_;
+    if (defined($failure_type)) {
+        if ($failure_type == DIE_ON_FAILURE) {
+            croak "**ERROR: $0 : " . $! . "\n";
+        } elsif ($failure_type == WARN_ON_FAILURE) {
+            carp "**WARNING: $0 : " . $! . "\n";
+        }
+    }
+}
+
+
+######################################################################
+# MISC
+
+sub printAtStart {
+print<<"EOF";
+---------------------------------------------------------------- 
+ $0
+ Copyright (C) Michael Imelfort
+    
+ This program comes with ABSOLUTELY NO WARRANTY;
+ This is free software, and you are welcome to redistribute it
+ under certain conditions: See the source for more details.
+---------------------------------------------------------------- 
+EOF
+}
+
+__DATA__
+
+=head1 NAME
+
+    explodeGenbank.pl
+
+=head1 COPYRIGHT
+
+   copyright (C) Michael Imelfort
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=head1 DESCRIPTION
+
+   Split multiple genbank records into separate files
+
+=head1 SYNOPSIS
+
+    explodeGenbank.pl  -genbank|g GB_FILE
+
+      -genbank -g GB_FILE          Genbank file to explode
+      [-help -h]                   Displays basic usage information
+         
+=cut
+
